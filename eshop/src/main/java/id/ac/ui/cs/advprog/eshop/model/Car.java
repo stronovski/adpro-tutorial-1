16276c1346017063c00ec47499710928a190dd79
stronovski <andisalsabilaardian@gmail.com>
2024-02-23 09:42:10 +0700

    Commit Car.java (real) (i overlooked something)

--- eshop/src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ eshop/src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -9,4 +9,36 @@ public class Car {
     private String carName;
     private String carColor;
     private int carQuantity;
+
+    public String getCarId() {
+        return this.carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    public String getCarName() {
+        return this.carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getCarColor() {
+        return this.carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public int getCarQuantity() {
+        return this.carQuantity;
+    }
+
+    public void setCarQuantity(int carQuantity) {
+        this.carQuantity = carQuantity;
+    }
 }
